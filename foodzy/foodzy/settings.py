4f8092020954bd661dbc43ecf7d176ac3b8e98bf
민Hieu <hieu1052k3@gmail.com>
2020-05-01 13:38:43 +0900

    changed host to domain
    .

--- foodzy/foodzy/settings.py
+++ foodzy/foodzy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')g$dhw1e0dg2&836)i57v+09v9q!6lovcta_*!(6n)@erslwtk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["34.64.81.90"]
+ALLOWED_HOSTS = ["ksahappy.com"]
 
 
 # Application definition
