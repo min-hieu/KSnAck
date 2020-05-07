152cd6f4f122faf3396e0917b8fbebde675c50de
jeinryu <18-078@ksa.hs.kr>
2020-05-07 19:39:17 +0900

    ranking system
    urls
    ranking.html
    views.py

--- foodzy/pages/views.py
+++ foodzy/pages/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from items.models import transaction
+<<<<<<< Updated upstream
 from items.forms import foodOfferForm, foodQueueForm
+=======
+from users.models import Student
+from django.db import models
+
+>>>>>>> Stashed changes
 
 def home_view(request):
-    return render(request, 'home.html', {})
+    rankings = Student.objects.order_by('-happiness', 'student_id')[:5]
+    context = {
+            'rankings' : rankings,
+            }
+    return render(request, 'home.html', context)
 
 def queue_view(request):
     transactions = transaction.objects.filter(status=3)
@@ -29,3 +39,11 @@ def queue_view(request):
     
     return render(request, 'pages/queue.html', context)
     
+def ranking_view(request):
+    rankings = Student.objects.order_by('-happiness', 'student_id')[:5]
+    context = {
+            'rankings' : rankings,
+            }
+    return render(request, 'ranking.html', context)
+
+
